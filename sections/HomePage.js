8b2312dcc4847e72b18081ab05b4ad77f979561a
ShivaShankarReddy098 <shankarreddyshiva83@gmail.com>
2024-12-13 20:14:02 +0530

    updated database

--- sections/HomePage.js
+++ sections/HomePage.js
@@ -3,7 +3,6 @@ import Image from "next/image";
 import imgSrc from "@/public/robot.avif";
 import LetterPullup from "@/components/ui/letter-pullup";
 import SlightFlip from "@/components/ui/flip-text";
-import { CoolMode } from "@/components/ui/cool-mode";
 import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
@@ -31,27 +30,6 @@ export default function HomePage() {
             </div>
           </div>
         </div>
-        {/* <Image src={imgSrc} className="-z-10 w-full"/>
-        <div className="z-10 absolute flex  flex-col">
-          <LetterPullup words="AI Interview Mocker" className="text-white" />
-          <SlightFlip
-            word="Elevate your site visibility with AI"
-            className="text-white"
-          />
-          <CoolMode>
-            <div className="flex items-center justify-center">
-              <Button
-                className="p-6  mt-6"
-                onClick={() => (window.location.href = "/dashboard")}
-              >
-                Get Started
-              </Button>
-            </div>
-          </CoolMode>
-          {/* <div className="z-10  relative left-20">
-            <p className="text-white ">Scroll down</p>
-          </div> */}
-        {/* </div> */}
       </section>
     </>
   );
