f5d88b27d043e2214ac6e7966dc20f03729da60f
ShivaShankarReddy098 <shankarreddyshiva83@gmail.com>
2024-12-14 19:24:52 +0530

    small change

--- app/dashboard/interview/[interviewId]/startInterview/_components/RecordAnswerSection.jsx
+++ app/dashboard/interview/[interviewId]/startInterview/_components/RecordAnswerSection.jsx
@@ -164,7 +164,7 @@ export default function RecordAnswerSection({
           </h2>
         )}
       </Button>
-      <p>{userAnswer || "Your answer will appear here..."}</p>
+      {/* <p>{userAnswer || "Your answer will appear here..."}</p> */}
     </div>
   );
 }
