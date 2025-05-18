0c36b7022ddf2ff5898f8655956b83a16fc0d8aa
ShivaShankarReddy098 <shankarreddyshiva83@gmail.com>
2025-05-18 18:58:59 +0530

    adding margin to lists

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -10,8 +10,8 @@ function Dashboard() {
       <h className="text-gray-500">Create and Start your AI Mockup Interview</h>
       <div className="grid grid-cols-1 md:grid-cols-3 my-5">
         <AddNewInterview />
+        <InterviewList />
       </div>
-      <InterviewList />
     </diV>
   );
 }
