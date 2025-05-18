c2c15bb56eb78e9bca14b2ec7cdca3c71e2cabe7
ShivaShankarReddy098 <shankarreddyshiva83@gmail.com>
2025-05-18 19:04:34 +0530

    adding margin to lists

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -5,13 +5,13 @@ import InterviewList from "./_components/InterviewList";
 
 function Dashboard() {
   return (
-    <diV className="p-10 lg:h-screen">
+    <diV className="p-10 lg:h-screen mb-5">
       <h1 className="font-bold text-2xl mt-24">Dashboard</h1>
       <h className="text-gray-500">Create and Start your AI Mockup Interview</h>
       <div className="grid grid-cols-1 md:grid-cols-3 my-5">
         <AddNewInterview />
-        <InterviewList />
       </div>
+      <InterviewList />
     </diV>
   );
 }
